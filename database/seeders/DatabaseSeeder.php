1733af8a86b501c13a5670b308ee6ab8ccca68ec
sandi <sandimaulidika@gmail.com>
2026-06-24 07:53:05 +0700

    feat: update DatabaseSeeder to use User factory and enhance user creation logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin'),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
 
         $this->call(CakeShopSeeder::class);
